b470894d9b5d5c87810eace89c35830d36f6e634
Bruno Alencar <joaobrunoalencar@gmail.com>
2019-05-21 19:32:06 -0300

    learning about yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,19 @@
 const getNotes = require('./notes')
-const log = console.log
 const chalk = require('chalk')
-log(chalk.black.bgGreenBright.bold.underline('Success!'))
+const yargs = require('yargs')
 
+
+// yargs.version('1.1.0')
+
+console.log(yargs.argv)
+// node app.js remove --title="bruno aprendendo"
+
+// create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function () {
+        console.log('adding new note') 
+    }
+})
+// add, remove, read, list
