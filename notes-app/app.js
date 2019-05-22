3b1c8bad2d08077d0c356adfd4ea049ccc800f95
Bruno Alencar <joaobrunoalencar@gmail.com>
2019-05-21 22:37:03 -0300

    doing a list

--- notes-app/app.js
+++ notes-app/app.js
@@ -5,15 +5,21 @@ const yargs = require('yargs')
 
 // yargs.version('1.1.0')
 
-console.log(yargs.argv)
 // node app.js remove --title="bruno aprendendo"
 
 // create add command
-yargs.command({
-    command: 'add',
-    describe: 'Add a new note',
-    handler: function () {
-        console.log('adding new note') 
-    }
+yargs.command('add', 'Add a new note', () => {
+    console.log('adding new note')
 })
+
+yargs.command('remove', 'remove a note', () => {
+    console.log('Removing a note')
+})
+
+
+yargs.command('list', 'list notes', () => { console.log('Showing all notes')})
+yargs.command('read', 'read a note', () => { console.log('Reading a single note') })
+
+console.log(yargs.argv) // need this to active yargs argv
+// yargs.command()
 // add, remove, read, list
