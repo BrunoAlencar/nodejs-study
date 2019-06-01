2f5fb9bf4d8a1698ef62435ae3635124b47bb654
Bruno Alencar <joaobrunoalencar@gmail.com>
2019-06-01 02:54:34 -0300

    request

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,10 @@
-console.log('Starting')
-
-setTimeout(() => {
-    console.log('2 seconds timer')
-}, 2000)
+ const request = require('request')
 
 
-setTimeout(() => {
-    console.log('0 seconds timer')
-}, 0)
+ const url = 'https://api.darksky.net/forecast/b4f4580e775b155a014fe2692295b158/37.8267,-122.4233'
 
-console.log('Stopping')
+ request({ url: url }, (error, response) => {
+    const data = JSON.parse(response.body)
+
+    console.log(data.currently)
+})
