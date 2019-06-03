4ce96e542fe25c0ca3ee48e40cfda6e8ac9ea038
Bruno Alencar <joaobrunoalencar@gmail.com>
2019-06-03 14:04:40 -0300

    static files html

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,22 +1,16 @@
+const path = require('path')
 const express = require('express')
 
 const app = express()
 
+app.use(express.static(path.join(__dirname, '../public')))
 
-app.get('', (req, res) => {
-    res.send('Hello world')
-})
-
-app.get('/help', (req, res) => {
-    res.send('Help page')
-})
-
-app.get('/about', (req, res) => {
-    res.send('About page')
-})
 
 app.get('/weather', (req, res)=> {
-    res.send('Weather page')
+    res.send({
+        forecast: 'Time is good',
+        location: 'Maceio'
+    })
 })
 
 
