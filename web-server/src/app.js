bef9123c3618a94674db22f8fa30521e34f66230
Bruno Alencar <joaobrunoalencar@gmail.com>
2019-06-03 21:11:00 -0300

    class 47 templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,11 +2,31 @@ const path = require('path')
 const express = require('express')
 
 const app = express()
-
 app.use(express.static(path.join(__dirname, '../public')))
 
+app.set('view engine', 'hbs')
 
-app.get('/weather', (req, res)=> {
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather app',
+        name: 'João Bruno Alencar'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'João Bruno Alencar'
+    })
+})
+
+app.get('/help', (req, res) => [
+    res.render('help', {
+        message: 'Hey, do you have any problem with this application?? Please contact me!'
+    })
+])
+
+app.get('/weather', (req, res) => {
     res.send({
         forecast: 'Time is good',
         location: 'Maceio'
