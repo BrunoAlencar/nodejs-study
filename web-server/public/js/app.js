7e556c123e83222ab9f4d9873e02759094cdf890
Bruno Alencar <joaobrunoalencar@gmail.com>
2019-06-06 11:50:16 -0300

    class 59

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -4,18 +4,26 @@ console.log('cliente side js file is loaded')
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+// messageOne.textContent = 'from javascript'
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
-    const location = search.value
 
+    messageTwo.textContent = ''
+    
+    const location = search.value
+    messageOne.textContent = 'Loading...'
     fetch('http://localhost:3000/weather?address=' + location).then(response => {
         response.json().then(data => {
+            messageOne.textContent = ''
             if (data.error) {
-                return console.log(data.error)
+                return messageTwo.textContent = data.error
             }
 
-
+            messageTwo.innerHTML = data.location + '<br>' + data.forecast.summary
             console.log(data.location)
             console.log(data.forecast)
         })
