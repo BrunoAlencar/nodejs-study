3c67b1722b13dce8f0949095b3c013bf8277f5c0
Bruno Alencar <joaobrunoalencar@gmail.com>
2019-06-06 12:10:09 -0300

    class 63

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -5,16 +5,16 @@ const forecast = (latitude, longitude, callback) => {
 
     const url = 'https://api.darksky.net/forecast/b4f4580e775b155a014fe2692295b158/' + latitude +','+ longitude + '?units=auto&lang=pt'
 
-    request({ url: url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, {body}) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
             callback(undefined, {
-                summary: response.body.daily.data[0].summary,
-                temperature: response.body.currently.temperature,
-                precipProbability: response.body.currently.precipProbability
+                summary: body.daily.data[0].summary,
+                temperature: body.currently.temperature,
+                precipProbability: body.currently.precipProbability
             })
         }
 
