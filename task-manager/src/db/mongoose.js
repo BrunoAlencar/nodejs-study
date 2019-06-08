bedb659d40533ea2e299b2a7befa06c210e9349b
Bruno Alencar <joaobrunoalencar@gmail.com>
2019-06-08 17:37:09 -0300

    class 86 data validation and sanitization

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,21 +1,55 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
+
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
 })
 
-// const User = mongoose.model('User', {
-//     name: {
-//         type: String
-//     },
-//     age: {
-//         type: Number
-//     }
-// })
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: [6, 'You need at least 6 characters at password'],
+        validate(value) {
+            if(value.toLowerCase().includes('password')){
+                throw new Error('Password can not have the text \"password\"')
+            }
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase:true,
+        validate(value) {
+            if(!validator.isEmail(value)){
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if(value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
+    },
+})
 
 // const me = new User({
-//     name: 'Bruno Alencar',
-//     age: 'mike'
+//     name: '  Mike  ',
+//     age: 1,
+//     email: 'aa@MAIil.com   ',
+//     password: 'PassaWord123'
 // })
 // me.save().then(result => {
 //     console.log(result)
@@ -33,17 +67,19 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const Task = mongoose.model('Task', {
     description: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     }
 })
 
 
 const task1 = new Task({
-    description: 'Study every single day!',
-    completed: false
+    description: 'Nooo hoo noo    ',
 }).save()
 .then(res => console.log(res))
 .catch(({message})=>  console.log(message))
