6492a08a46562ad88add3125da1de6042e8fa24a
Bruno Alencar <joaobrunoalencar@gmail.com>
2019-06-07 21:30:23 -0300

    class 76

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -13,8 +13,52 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({
-        name: 'Bruno',
-        age: 23
+    // db.collection('users').insertOne({
+    //     name: 'Bruno',
+    //     age: 23
+    // }, (error, result) => {
+    //     if(error){
+    //         return console.log('Unable to insert user', error)
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Jen',
+    //         age: 28
+    //     }, {
+    //         name: 'Gunter',
+    //         age: 26
+    //     }
+    // ], (error, result) => {
+    //     if(error) {
+    //         return console.log('Unable to insert documents', error)
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'Study one hour per day',
+            completed: true
+        },
+        {
+            description: 'Do exercises',
+            completed: true
+        },
+        {
+            description: 'Eat well',
+            completed: false
+        }
+    ], (error, result) => {
+        if(error){
+            return console.log('Unable to insert tasks documents', error)
+        }
+
+        console.log(result.ops)
     })
+
 })
