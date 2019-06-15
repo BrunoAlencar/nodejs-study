377e73984fd8ae9c2603d6ffc75d769f8a637d9c
Bruno Alencar <joaobrunoalencar@gmail.com>
2019-06-14 22:06:43 -0300

    working with middlewares

--- task-manager/src/app.js
+++ task-manager/src/app.js
@@ -6,8 +6,19 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(express.json())
+// app.use((req, res, next) => {
+//     if(req.method === 'GET'){
+//         res.send('GET requests are disabled')
+//     }else {
+//         next()
+//     }
+// })
 
+app.use((req, res, next)=> {
+    res.status(503).send('The website is under maintenance, please try again later! Sorry :/')
+})
+
+app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
