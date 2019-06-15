94a1e8313b00411139ae83c3cafdef65c2b0b1c4
Bruno Alencar <joaobrunoalencar@gmail.com>
2019-06-14 22:56:17 -0300

    auth middleware with jasonWebToken

--- task-manager/src/app.js
+++ task-manager/src/app.js
@@ -3,20 +3,11 @@ require('./db/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
+
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.use((req, res, next) => {
-//     if(req.method === 'GET'){
-//         res.send('GET requests are disabled')
-//     }else {
-//         next()
-//     }
-// })
 
-app.use((req, res, next)=> {
-    res.status(503).send('The website is under maintenance, please try again later! Sorry :/')
-})
 
 app.use(express.json())
 app.use(userRouter)
