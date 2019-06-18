368b7ae70adb793b32bbebd9b1e3566406f39331
Bruno Alencar <joaobrunoalencar@gmail.com>
2019-06-18 08:35:12 -0300

    public data only

--- task-manager/src/app.js
+++ task-manager/src/app.js
@@ -1,36 +1,17 @@
-const express = require('express')
-require('./db/mongoose')
-const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
+const express = require("express");
+require("./db/mongoose");
+const userRouter = require("./routers/user");
+const taskRouter = require("./routers/task");
 
+const app = express();
+const port = process.env.PORT || 3000;
 
-const app = express()
-const port = process.env.PORT || 3000
-
-
-
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
-
-
-
+app.use(express.json());
+app.use(userRouter);
+app.use(taskRouter);
 
 app.listen(port, () => {
-    console.log('Server is on the port: ' + port)
-})
+  console.log("Server is on the port: " + port);
+});
 
 // https://httpstatuses.com/
-
-
-// const jwt = require('jsonwebtoken')
-
-// const myFunction = async () => {
-//     const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse', {
-//         expiresIn: '7 days'
-//     })
-//     console.log(token)
-
-//     console.log(jwt.verify(token, 'thisismynewcourse'))
-// }
-// myFunction()
