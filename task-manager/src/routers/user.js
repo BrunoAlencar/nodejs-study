142d00835e73897e7c0bc8ae3cb86cceea868a98
Bruno Alencar <joaobrunoalencar@gmail.com>
2019-06-19 16:40:07 -0300

    class 129 file uploads cropping and image formatting

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const multer = require("multer");
+const sharp = require("sharp");
 const User = require("../models/user");
 const auth = require("../middleware/auth");
-const multer = require("multer");
 const router = new express.Router();
 
 router.post("/users", async (req, res) => {
@@ -101,7 +102,6 @@ router.delete("/users/me", auth, async (req, res) => {
 });
 
 const upload = multer({
-  dest: "avatars",
   limits: {
     fileSize: 1000000
   },
@@ -118,12 +118,44 @@ router.post(
   "/users/me/avatar",
   auth,
   upload.single("avatar"),
-  (req, res) => {
+  async (req, res) => {
+    const buffer = await sharp(req.file.buffer)
+      .resize({ width: 250, height: 250 })
+      .png()
+      .toBuffer();
+
+    req.user.avatar = buffer;
+    await req.user.save();
     res.send();
   },
   ({ message: error }, req, res, next) => {
     res.status(400).send({ error });
   }
 );
+
+router.delete("/users/me/avatar", auth, async (req, res) => {
+  try {
+    req.user.avatar = undefined;
+    await req.user.save();
+    res.send();
+  } catch ({ message: error }) {
+    res.status(500).send({ error });
+  }
+});
+
+router.get("/users/:id/avatar", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user || !user.avatar) {
+      throw new Error("No user or avatar");
+    }
+
+    res.set("Content-Type", "image/png");
+    res.send(user.avatar);
+  } catch ({ message: error }) {
+    res.status(404).send({ error });
+  }
+});
 
 module.exports = router;
