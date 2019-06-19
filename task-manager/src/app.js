5b310841754d6f3dd5cf7519ecf9605a4ec58b77
Bruno Alencar <joaobrunoalencar@gmail.com>
2019-06-19 09:02:50 -0300

    class 118 starting new section

--- task-manager/src/app.js
+++ task-manager/src/app.js
@@ -16,16 +16,16 @@ app.listen(port, () => {
 
 // https://httpstatuses.com/
 
-const Task = require("./models/task");
-const User = require("./models/user");
+// const Task = require("./models/task");
+// const User = require("./models/user");
 
-const main = async () => {
-  // const task = await Task.findById("5d093d5c49935b2ef8c6b8a3");
-  // await task.populate("owner").execPopulate();
-  // console.log(task.owner);
-  const user = await User.findById("5d08fcd9a0645004bc0c7a92");
-  await user.populate("tasks").execPopulate();
-  console.log(user.tasks);
-};
+// const main = async () => {
+//   // const task = await Task.findById("5d093d5c49935b2ef8c6b8a3");
+//   // await task.populate("owner").execPopulate();
+//   // console.log(task.owner);
+//   const user = await User.findById("5d08fcd9a0645004bc0c7a92");
+//   await user.populate("tasks").execPopulate();
+//   console.log(user.tasks);
+// };
 
-main();
+// main();
