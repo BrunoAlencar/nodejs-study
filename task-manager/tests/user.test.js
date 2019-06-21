4a921ddddc5323d85cb42db379bb93a95bc1a8d6
Bruno Alencar <joaobrunoalencar@gmail.com>
2019-06-21 07:39:08 -0300

    class 144 learning how to do test with an api using express

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -1,11 +1,19 @@
 const request = require('supertest')
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
 const app = require('../src/app')
 const User = require('../src/models/user')
 
+
+const userOneId = new mongoose.Types.ObjectId()
 const userOne = {
+    _id: userOneId,
     name: 'Mike',
     email: 'mike@mail.com',
-    password: '123456'
+    password: '123456',
+    tokens: [{
+        token: jwt.sign({ _id: userOneId }, process.env.JSON_WEB_TOKEN_KEY)
+    }]
 }
 
 beforeEach(async () => {
@@ -35,4 +43,34 @@ test('Should login existing user', async () => {
 
 test('Should not login nonexistent user', async () => {
     await request(app).post('/users/login').send({}).expect(400)
+})
+
+test('Should get profile for user', async () => {
+    await request(app)
+        .get('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+})
+
+test('Should not get profile for unauthenticated user', async () => {
+    await request(app)
+    .get('/users/me')
+    .send()
+    .expect(401)
+})
+
+test('Should delete a account for user', async ()=> {
+    await request(app)
+    .delete('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200)
+})
+
+test('Should not delete user', async () => {
+    await request(app)
+    .delete('/users/me')
+    .send()
+    .expect(401)
 })
